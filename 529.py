eba5fefcede7c5464849607ae65798b4937cd6ff
Derek <dekey@me.com>
2014-12-03 10:50:28 -0500

    Added lookup tables (LUTs) for Maryland state tax as well as county tax rates. Since the rates can be quite high, using these more accurate tax rates has been having a very real impact on the results from these simulations.

--- 529.py
+++ 529.py
@@ -27,18 +27,65 @@ path = os.path.dirname(__file__)
 HISTORICAL_DATA         = os.path.join(path, 'historical_market_index_data.csv')
 OUTFILE_NAME            = os.path.join(path, 'histogram.pdf') 
 
+# tax bracket LUTs
+MD_STATE_TAX_2014 = {'Single':  { '$0+'       : .0200,
+                                  '$1,000'    : .0300,
+                                  '$2,000'    : .0400,
+                                  '$3,000'    : .0475,
+                                  '$100,000+' : .0500,
+                                  '$125,000+' : .0525,
+                                  '$150,000+' : .0550,
+                                  '$200,000+' : .0575},
+                     'Couple':  { '$0+'       : .0200,
+                                  '$1,000'    : .0300,
+                                  '$2,000'    : .0400,
+                                  '$3,000'    : .0475,
+                                  '$150,000+' : .0500,
+                                  '$175,000+' : .0525,
+                                  '$225,000+' : .0550,
+                                  '$300,000+' : .0575}
+                    }
+
+MD_COUNTY_TAX_2014 = {  'Allegany County'       : .0305,
+                        'Anne Arundel County'   : .0256,
+                        'Baltimore'             : .0305,
+                        'Baltimore County'      : .0283,
+                        'Calvert County'        : .0280,
+                        'Caroline County'       : .0263,
+                        'Carroll County'        : .0305,
+                        'Cecil County'          : .0280,
+                        'Charles County'        : .0290,
+                        'Dorchester County'     : .0262,
+                        'Frederick County'      : .0296,
+                        'Garrett County'        : .0265,
+                        'Harford County'        : .0306,
+                        'Howard County'         : .0320,
+                        'Kent County'           : .0285,
+                        'Montgomery County'     : .0320,
+                        'Prince Georges County' : .0320,
+                        'Queen Annes County'    : .0285,
+                        'Somerset County'       : .0315,
+                        'St. Marys County'      : .0300,
+                        'Talbot County'         : .0225,
+                        'Washington County'     : .0280,
+                        'Wicomico County'       : .0310,
+                        'Worcester County'      : .0125}
+
+
 # choose the index you want to use for the simulation.
 # Options are: 'DJIA', 'Wilshire_5000', 'S&P500',
 #              '3_Month_Treasury', '10_Year_Treasury'
 INDEX                   = '10_Year_Treasury'
 SIMULATIONS             = 100000
-STARTING_INVESTMENT     = 20000.
-NUM_OF_YEARS            = 10
-ANNUAL_INVESTMENT       = 5000.
+STARTING_INVESTMENT     = 0.
+NUM_OF_YEARS            = 18
+ANNUAL_INVESTMENT       = 2500.
 PLAN_1_EXPENSE_RATIO    = 0.0088
-PLAN_1_TAX_BENEFIT      = 0.0475
+PLAN_1_TAX_BENEFIT      = MD_STATE_TAX_2014['Couple']['$150,000+'] + \
+                          MD_COUNTY_TAX_2014['Montgomery County']
 PLAN_2_EXPENSE_RATIO    = 0.0019
 PLAN_2_TAX_BENEFIT      = 0.
+
 
 
 # import historical market index data from csv file
@@ -46,15 +93,15 @@ with open(HISTORICAL_DATA, 'rb') as csvfile:
     rows = csv.DictReader(csvfile)
     historical_performances = [row[INDEX] for row in rows if row[INDEX] <> '']
 
-print '               Starting Investment: ${STARTING_INVESTMENT:,.0f}\n'\
-      '       Annual Investment into Plan: ${ANNUAL_INVESTMENT:,.0f}\n'\
-      '                   Number of Years: {NUM_OF_YEARS} years\n'\
-      '              Plan 1 Expense Ratio: {PLAN_1_EXPENSE_RATIO:.2%}\n'\
-      '                Plan 1 Tax Benefit: {PLAN_1_TAX_BENEFIT:.2%}\n'\
-      '              Plan 2 Expense Ratio: {PLAN_2_EXPENSE_RATIO:.2%}\n'\
-      '                Plan 2 Tax Benefit: {PLAN_2_TAX_BENEFIT:.2%}\n'\
-      '             Historical Index Used: {INDEX}\n\n'\
-      '                     Calculating... {SIMULATIONS:,} simulations'\
+print '                   Starting Investment: ${STARTING_INVESTMENT:,.0f}\n'\
+      '           Annual Investment into Plan: ${ANNUAL_INVESTMENT:,.0f}\n'\
+      '                       Number of Years: {NUM_OF_YEARS} years\n'\
+      '                  Plan 1 Expense Ratio: {PLAN_1_EXPENSE_RATIO:.2%}\n'\
+      '                    Plan 1 Tax Benefit: {PLAN_1_TAX_BENEFIT:.2%}\n'\
+      '                  Plan 2 Expense Ratio: {PLAN_2_EXPENSE_RATIO:.2%}\n'\
+      '                    Plan 2 Tax Benefit: {PLAN_2_TAX_BENEFIT:.2%}\n'\
+      '                 Historical Index Used: {INDEX}\n\n'\
+      '                         Calculating... {SIMULATIONS:,} simulations'\
       ''.format(**locals())
 
 plan_1_results = []
@@ -82,14 +129,16 @@ for i, result in enumerate(differences):
     if result > 0:
         likelihood = 1-i/float(len(differences))
         break
-        
-print '        Plan 1 Median Final Amount: ${p1_median:,.0f} '\
+else:
+    likelihood = 0.
+
+print '            Plan 1 Median Final Amount: ${p1_median:,.0f} '\
       '(95% HPD: ${p1_hpd_low:,.0f} to ${p1_hpd_high:,.0f})\n'\
-      '        Plan 2 Median Final Amount: ${p2_median:,.0f} '\
+      '            Plan 2 Median Final Amount: ${p2_median:,.0f} '\
       '(95% HPD: ${p2_hpd_low:,.0f} to ${p2_hpd_high:,.0f})\n'\
-      '                 Median Difference: ${diff_median:,.0f} '\
+      '                     Median Difference: ${diff_median:,.0f} '\
       '(95% HPD: ${diff_hpd_low:,.0f} to ${diff_hpd_high:,.0f})\n'\
-      '  Probability that Plan 2 > Plan 1: {likelihood:.2%}'\
+      '   Likelihood that Plan 2 beats Plan 1: {likelihood:.2%}'\
       ''.format(p1_median = plan_1_results[int(0.5*SIMULATIONS)],
                 p1_hpd_low = plan_1_results[int(0.025*SIMULATIONS)],
                 p1_hpd_high = plan_1_results[int(0.975*SIMULATIONS)],
